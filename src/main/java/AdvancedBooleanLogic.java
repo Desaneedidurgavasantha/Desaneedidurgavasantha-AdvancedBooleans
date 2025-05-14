b4f94c629e31f85fcda85393b514f92833bdac06
Desaneedidurgavasantha <dedurga132@gmail.com>
2025-05-14 18:01:45 +0530

    YES

--- src/main/java/AdvancedBooleanLogic.java
+++ src/main/java/AdvancedBooleanLogic.java
@@ -37,9 +37,7 @@ public class AdvancedBooleanLogic {
 }
  /**
      * Neither b1 NOR b2
-     * @param b1 first boolean to be compared.
-     * @param b2 second boolean to be compared.
-     * @return true if neither b1 nor b2 are true. Return false if either b1 or b2 is true.
+     *ther b1 nor b2 are true. Return false if either b1 or b2 is true.
      */
 
 
